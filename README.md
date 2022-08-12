6c3343fb90b90342cc9e781ca9b1866cf372f26e
Joel-De <71618484+Joel-De@users.noreply.github.com>
2022-08-11 23:50:03 -0400

    Added basic tensorboard functionality
    Added tensorboard to view Model grpah, training loss and validation accuracy

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PneumoniaDetection
  Detecting presence of Pneumonia via X-ray images
+
+
+
+
+# Tensorboard Bug
+If on Windows and tensorboard is rendering as a white page, follow the instructions present [here](https://github.com/tensorflow/tensorboard/issues/3117#issuecomment-605531669), this is a bug with tensorflow.
--- train.py
+++ train.py
@@ -4,33 +4,36 @@ import time
 
 import numpy as np
 import torch
-import torch.optim as optim
+import tensorboard
+from torch import optim
 from torch.utils.data import DataLoader
 from torchvision.models import vision_transformer
 from tqdm import tqdm
 from torch.optim.lr_scheduler import StepLR
+from torch.utils.tensorboard import SummaryWriter
 from data.dataset import PneumoniaDetectionDataset
-
-
+from datetime import datetime
+from statistics import fmean
 
 def parseArgs():
     p = argparse.ArgumentParser()
     p.add_argument("--data", help="Path to data parent directory", required=True)
     p.add_argument("--device", default="cuda", help="Device to use for training")
-    p.add_argument("--img_size", default=224, help="Image size to train the model at")
+    p.add_argument("--img_size", default=64, help="Image size to train the model at")
     p.add_argument("--epochs", default=60, help="Number of epochs to train for")
     p.add_argument("--val_freq", default=10, help="How many epochs between validations")
     p.add_argument("--save_frequency", default=10, help="How many epochs between model save")
     p.add_argument("--save_dir", default="checkpoints", help="Location of where to save model")
     p.add_argument("--load_model", type=str, default=None,
                    help="Location of where the model you want to load is stored")
-
     p.add_argument("--batch_size", type=int, help="Batch-size to train with", default=4)
     p.add_argument("--num_workers", type=int, help="Number of workers to use for dataloading", default=4)
     p.add_argument("--lr_step_size", type=int, default=30, help="How often to decay learning rate")
     p.add_argument("--lr_gamma", type=float, default=0.1, help="Factor to decrease learning rate by")
     p.add_argument("--lr", type=float, default=0.001, help="Learning rate to use in training")
     p.add_argument("--momentum", type=float, default=0.9, help="Momentum of learning rate for ADAM")
+    p.add_argument("--name", type=str, default=f"Train_{datetime.now().strftime('%m_%d_%H_%M_%S')}", help="Name to save results under")
+
     args = p.parse_args()
     return args
 
@@ -63,8 +66,7 @@ if __name__ == '__main__':
     if args.load_model and os.path.exists(args.load_model):
         model.load_state_dict(torch.load(args.load_model)["model"])
         print(f"Loaded {args.load_model}")
-    model.to(args.device)
-    model.train()
+
 
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), args.lr, momentum=args.momentum)
@@ -73,8 +75,15 @@ if __name__ == '__main__':
 
     time.sleep(1)
     iterCount = 0
-
-
+    print("Starting Tensorboard server")
+    tensorBoardProgram = tensorboard.program.TensorBoard()
+    tensorBoardProgram.configure(argv=[None, '--logdir', "tensorboard_runs"])
+    url = tensorBoardProgram.launch()
+    print(f"Launched Tensorboard server at {url}")
+    tensorboardWriter = SummaryWriter(log_dir=f"tensorboard_runs/{args.name}")
+    tensorboardWriter.add_graph(model, torch.zeros((1,3,args.img_size,args.img_size)))
+    model.to(args.device)
+    model.train()
     def evalModel(dataLoader: DataLoader):
         """
         :param dataLoader: DataLoader instance of the set you wish to evaluate on
@@ -106,6 +115,7 @@ if __name__ == '__main__':
     for epoch in range(1, args.epochs + 1, 1):
         print(f"Starting epoch {epoch}")
         time.sleep(1)
+        runningloss = []
         for data in tqdm(trainSetDataLoader):
             batch, label = data['image'], data['class']
             batch = batch.to(args.device)
@@ -118,7 +128,7 @@ if __name__ == '__main__':
 
             loss = loss.to(torch.float32)
             loss_ = criterion(loss, label)
-
+            runningloss.append(loss_)
             scaler.scale(loss_).backward()
             scaler.step(optimizer)
             scaler.update()
@@ -132,13 +142,18 @@ if __name__ == '__main__':
             accuracy = evalModel(
                 testSetDataLoader)  # Grouping test set here since validation set is much smaller, test set not used for any hyper-param optimizations
             print(f"Accuracy after test is {accuracy * 100}%")
+            tensorboardWriter.add_scalar("Accuracy/Val", accuracy, global_step=epoch)
 
         if epoch % args.save_frequency == 0:
-            saveDir = os.path.join(args.save_dir, "Model.pth")
+            saveDir = os.path.join(args.save_dir, f"{args.name}_model.pth")
             modelDict = {
                 "model": model.state_dict(),
                 "imgSize": args.img_size
             }
             torch.save(modelDict, saveDir)
             print(f"Saved Model to {saveDir}")
+        tensorboardWriter.add_scalar("Val/Loss", fmean(runningloss), global_step=epoch)
+
+
+    tensorboardWriter.close()
     print('Finished Training')
