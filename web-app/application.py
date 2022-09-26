3dc138a1322002cf0cc256b22fd8330abae32d61
Joel-De <71618484+Joel-De@users.noreply.github.com>
2022-09-26 18:28:59 -0400

    Update application.py

--- web-app/application.py
+++ web-app/application.py
@@ -2,23 +2,23 @@ import argparse
 import io
 import os
 import sys
+
 sys.path.append(os.path.join(os.getcwd()))
 sys.path.append(os.path.join(os.getcwd(), '..'))
 import torch
 from PIL import Image
-from flask import Flask, render_template, url_for, jsonify
+from flask import Flask, render_template, jsonify
 from flask import request, redirect
 from model import PneumoniaDetectionModel
 from data.dataset import PneumoniaDetectionDataset
 
-
-app = Flask(__name__,template_folder='.')
+app = Flask(__name__, template_folder='.')
 app.secret_key = b'@VOv3oactreto8yavheE$B^eo'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    model, args, modelData = loadModel()
+    model, modelData, device = loadModel()
     if request.method == 'POST':
         if 'file' not in request.files:
             print('No file part')
@@ -29,12 +29,13 @@ def upload_file():
         img = Image.open(io.BytesIO(file.read())).convert('RGB')
         img.save("static/tmp.png")
         img = PneumoniaDetectionDataset.basicPreprocess(modelData['imgSize'])(img)
-        img = img.type(torch.FloatTensor).unsqueeze(0).to(args.device)
+        img = img.type(torch.FloatTensor).unsqueeze(0).to(device)
         result = model(img)
         outputClass = PneumoniaDetectionDataset.getClassMap()[result.argmax().item()]
         printMessage = f"The patient has {outputClass}" if outputClass == "Pneumonia" else f"The patient is healthy!"
         return jsonify(printMessage)
     return render_template('./index.html')
+
 
 def parseArgs():
     p = argparse.ArgumentParser()
@@ -46,15 +47,21 @@ def parseArgs():
 
 
 def loadModel():
-    args = parseArgs()
-    modelData = torch.load(args.load_model, map_location=torch.device('cpu'))
+    # args = parseArgs()
+    # load_model = args.load_model
+    # device = args.device
+
+    load_model = "static/prod.pth"
+    device = "cpu"
+
+    modelData = torch.load(load_model, map_location=torch.device('cpu'))
     model = PneumoniaDetectionModel()
     model.load_state_dict(modelData['model'])
-    print(f"Loaded {args.load_model}")
-    model.to(args.device)
+    print(f"Loaded {load_model}")
+    model.to(device)
     model.eval()
-    return model, args, modelData
+    return model, modelData, device
+
 
 if __name__ == '__main__':
-    model, args, modelData = loadModel()
-    app.run(debug=True, port=os.getenv('PORT',5000))
+    app.run(debug=True, port=os.getenv('PORT', 5000))
